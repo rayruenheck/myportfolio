5982ee41e6c34d373e46aac26c9a6b6b2a0c727c
rayruenheck <rayruenheck@gmail.com>
2024-01-19 16:03:23 -0500

    apostrophe

--- src/app/compontents/hero.tsx
+++ src/app/compontents/hero.tsx
@@ -6,11 +6,11 @@ export default function Hero() {
   return (
     <div id='objective' className='flex flex-col md:flex-row w-full justify-center items-center h-[100vh] p-10'>
       <div className='w-full md:w-1/2 flex flex-col justify-center p-6 text-center md:text-left'>
-        <p className='text-4xl lg:text-7xl mb-5 font-bold'>Hi, I'm Ray.</p>
+        <p className='text-4xl lg:text-7xl mb-5 font-bold'>Hi, I&apos;m Ray.</p>
         <p className='text-lg'>
         Driven individual with over five years in high-pressure customer service environments, seeking to leverage transferable
 skills in communication, problem-solving, and teamwork to transition to an entry-level role as a software developer.
-Passionate about learning and growth, I've established a foundation of full-stack skills through the Coding Temple
+Passionate about learning and growth, I&apos;ve established a foundation of full-stack skills through the Coding Temple
 bootcamp and personal projects. Eager to collaborate and apply technical skills to solve real-world business needs.
         </p>
       </div>
